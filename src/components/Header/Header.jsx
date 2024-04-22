0876b631357049cc86f763b3375974f9aa00a216
abunaim5 <abunaim140@gmail.com>
2024-04-23 00:47:57 +0600

    created basic react file with router and added root, home, header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../../../public/images/more/logo1.png';
+import background from '../../../public/images/more/15.jpg';
+
+const Header = () => {
+    return (
+        <div style={{backgroundImage: `url(${background})`}} className="flex items-center justify-center py-6 bg-cover bg-center object-contain bg-no-repeat">
+            <div className="flex items-center gap-4">
+                <img className='w-[75px]' src={logo} alt="" />
+                <h1 className="font-rancho text-6xl text-[#FFF]">Espresso Emporium</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Root from './Layout/Root.jsx';
+import Home from './Pages/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    // errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
