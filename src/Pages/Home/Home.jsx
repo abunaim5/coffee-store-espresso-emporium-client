1607f700fde742f534e026bce9dccb2b542a5987
abunaim5 <abunaim140@gmail.com>
2024-04-23 01:47:30 +0600

    added follow on instagram component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import FollowOnInstagram from "../../components/FollowOnInstagram/FollowOnInstagram";
 import Quality from "../../components/Quality/Quality";
 
 const Home = () => {
     return (
         <div>
             <Quality></Quality>
+            <div className="mt-28">
+                <FollowOnInstagram></FollowOnInstagram>
+            </div>
         </div>
     );
 };
---
+++ src/components/FollowOnInstagram/FollowOnInstagram.jsx
@@ -0,0 +1,23 @@
+
+const FollowOnInstagram = () => {
+    return (
+        <div className="max-w-7xl mx-auto">
+            <div className="text-center space-y-2 mb-12">
+                <p className="text-xl text-[#1B1A1A]">Follow Us Now</p>
+                <h4 className="font-rancho text-[55px] text-[#331A15]">Follow on Instagram</h4>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                <img src="https://i.postimg.cc/43zLxCWW/Rectangle-9.png" alt="" />
+                <img src="https://i.postimg.cc/RCPPFLTx/Rectangle-10.png" alt="" />
+                <img src="https://i.postimg.cc/pr1k6PdL/Rectangle-11.png" alt="" />
+                <img src="https://i.postimg.cc/1tBvDGh6/Rectangle-12.png" alt="" />
+                <img src="https://i.postimg.cc/Qxw0gNP6/Rectangle-13.png" alt="" />
+                <img src="https://i.postimg.cc/Vkt4KQPz/Rectangle-14.png" alt="" />
+                <img src="https://i.postimg.cc/85NyFy3N/Rectangle-15.png" alt="" />
+                <img src="https://i.postimg.cc/5ytngXbr/Rectangle-16.png" alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default FollowOnInstagram;
